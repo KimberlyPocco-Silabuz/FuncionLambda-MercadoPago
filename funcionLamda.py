407003281bcd8a4589ab4818cd6ccff3aee715b3
KimberlyPocco <kimberly.poccop@gmail.com>
2023-01-16 19:24:19 -0500

    add code  process_payment

---
+++ funcionLamda.py
@@ -0,0 +1,25 @@
+import json
+import os
+import mercadopago 
+
+
+def lambda_handler(event, context):
+    #trabajamos con checkoutbrick usando el SDK de mercadopago
+    sdk= mercadopago.SDK(os.environ["ACCESS_TOKEN"])
+
+    #event traerá los datos object JSON
+    bodyGet = event
+    
+    #cargara los datos de cardFormData
+    payment_response = sdk.payment().create(bodyGet)
+    payment = payment_response["response"]
+    
+    #cargara los datos de items.products
+    #preference_response = sdk.preference().create(bodyGet["productData"])
+    #preference = preference_response["response"]
+
+    #se retorna una respuesta de la generación del pago
+    return {
+        'statusCode': 200,
+        'body': json.dumps(payment)
+    }
